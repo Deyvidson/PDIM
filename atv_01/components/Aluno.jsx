7a12645327303c114f5494b70e1850d62352d320
Deyvidson <deyvidson98@gmail.com>
2021-10-08 20:05:55 -0300

    atv_01

---
+++ atv_01/components/Aluno.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from "react"
+import {Text,View} from 'react-native'
+
+
+class Aluno extends Component {
+    render(){
+        return(
+            <View>
+                <Text>Deyvidson Lima</Text>
+                <Text>21 anos</Text>
+                <Text>Jaguaribe-CE</Text>
+                <Text>Design Digital</Text>
+            </View>
+        )
+    }
+}
+
+export default Aluno
